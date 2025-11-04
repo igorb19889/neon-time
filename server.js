8a717938bbab6183114a173e899a04fe76b15a53
Igor Balinschii <igorbalinschi8@gmail.com>
2025-11-04 17:02:15 +0200

    Update Stripe success/cancel URLs

--- server.js
+++ server.js
@@ -32,10 +32,7 @@ app.post("/create-checkout-session", async (req, res) => {
         },
       ],
       mode: "payment",
-      // success_url: "http://localhost:5173/contact?payment=success",
-      // cancel_url: "http://localhost:5173/contact?payment=cancel",
-
-
+     
 
       success_url: "https://neon-time-ww3j.vercel.app/contact?payment=success",
       cancel_url:  "https://neon-time-ww3j.vercel.app/contact?payment=cancel",
@@ -44,11 +41,7 @@ app.post("/create-checkout-session", async (req, res) => {
 
 
 
-      // success_url: 'http://192.168.100.47:5173/contact?payment=success',
-      // cancel_url: 'http://192.168.100.47:5173/contact?payment=cancel',
-
-      // success_url: "http://192.168.100.47:5173/contact?payment=success#reservation_submit",
-      // cancel_url: "http://192.168.100.47:5173/contact?payment=cancel#reservation_submit",
+  
     });
 
 
